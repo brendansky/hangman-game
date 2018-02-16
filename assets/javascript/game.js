0d424f06f7d6e5b1b39dfc2e684762318fe9d197
brendansky <33739060+brendansky@users.noreply.github.com>
2018-02-15 22:30:52 -0600

    javascript to create random answer and letters

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+alert("hello world!");
+
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+
+for (var i = 0; i < letters.length; i++) {
+
+    var button = document.createElement("button");
+    button.classList.add("btn", "btn-primary");
+    button.innerHTML = letters[i];
+
+    var buttons = document.getElementById("buttons");
+    buttons.appendChild(button);
+};
+
+
+var possibleAnswer = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+var n = (Math.floor(Math.random() * 10));
+alert(possibleAnswer[n]);
+
+
+var answer = possibleAnswer[n];
+console.log(answer);
+
+for (var i = 0; i < answer.length; i++) {
+    hiddenAnswer = document.createElement("answer");
+    hiddenAnswer.classList.add("letter");
+    hiddenAnswer.innerHTML = answer[i];
+
+    var createAnswer = document.getElementById("hidden-answer");
+    createAnswer.appendChild(hiddenAnswer);
+
+
+}
+
+
+
+
+
+
+
