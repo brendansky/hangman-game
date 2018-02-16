c1f04e5d07391b74a3fd73c4b10e75cb4fb63e1a
brendansky <33739060+brendansky@users.noreply.github.com>
2018-02-15 23:24:35 -0600

    added on-clicks to letter buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,5 @@
 alert("hello world!");
 
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
-
-for (var i = 0; i < letters.length; i++) {
-
-    var button = document.createElement("button");
-    button.classList.add("btn", "btn-primary");
-    button.innerHTML = letters[i];
-
-    var buttons = document.getElementById("buttons");
-    buttons.appendChild(button);
-};
-
-
 var possibleAnswer = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
 var n = (Math.floor(Math.random() * 10));
@@ -30,8 +17,47 @@ for (var i = 0; i < answer.length; i++) {
     var createAnswer = document.getElementById("hidden-answer");
     createAnswer.appendChild(hiddenAnswer);
 
+}
+
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+
+for (var i = 0; i < letters.length; i++) {
+
+    var button = document.createElement("button");
+    button.classList.add("btn", "btn-primary");
+    button.innerHTML = letters[i];
+
+    var buttons = document.getElementById("buttons");
+    buttons.appendChild(button);
+
+};
+
+var userChoice = document.getElementsByClassName( "btn" );
+console.log (userChoice)
+
+for (var i = 0; i < userChoice.length; i++){
+    userChoice[i].addEventListener("click", function(){
+        console.log("hello world")
+    })
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
