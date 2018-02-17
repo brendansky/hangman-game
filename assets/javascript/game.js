3c77549bb0576ac964257860d18bd7be60fd79cd
brendansky <33739060+brendansky@users.noreply.github.com>
2018-02-16 20:27:36 -0600

    working simple hangman game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // array of all possible answers //
 
-var possibleAnswer = ["ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"];
+var possibleAnswer = ["ZERO", "ONE", "TWO", "THREE", "FOUR", "INCREDIBLE", "SIX", "SEVEN", "EIGHT", "NINE"];
 
 // chooses a random number 0-9 //
 var n = (Math.floor(Math.random() * 10));
@@ -15,7 +15,7 @@ console.log(answer);
 for (var i = 0; i < answer.length; i++) {
     hiddenAnswer = document.createElement("answer");
     hiddenAnswer.classList.add("letter", "hidden");
-    hiddenAnswer.setAttribute( "id", "answer" + i )
+    hiddenAnswer.setAttribute("id", "answer" + i)
     hiddenAnswer.innerHTML = answer[i];
 
     var createAnswer = document.getElementById("hidden-answer");
@@ -24,14 +24,14 @@ for (var i = 0; i < answer.length; i++) {
 
 // creates a button for each letter // 
 
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ];
 
 for (var i = 0; i < letters.length; i++) {
 
     var button = document.createElement("button");
-    button.classList.add("btn", "btn-primary",);
+    button.classList.add("btn", "btn-primary", );
     button.innerHTML = letters[i];
-    
+
 
 
     var buttons = document.getElementById("buttons");
@@ -41,64 +41,83 @@ for (var i = 0; i < letters.length; i++) {
 
 // creates an on-click for each letter-button //
 
-var userChoice = document.getElementsByClassName( "btn" );
+var userChoice = document.getElementsByClassName("btn");
+
+var correctTotal = 0;
+var wrongTotal = 0;
+var correctThisRound = 0;
+var wrongThisRound = 0;
 
 
-for (var i = 0; i < userChoice.length; i++){
-    userChoice[i].addEventListener("click", function(){
+
+for (var i = 0; i < userChoice.length; i++) {
+    userChoice[i].addEventListener("click", function () {
 
         // hides the button //    
         this.classList.add("invisible");
-
-
         console.log(this.innerHTML);
-        for( i = 0; i < answer.length; i++){
-            if( this.innerHTML === answer[i]){
-                console.log ( "correct" );
-                
-                var reveal = document.getElementById( "answer" + i );
-                console.log (reveal);
-                reveal.classList.remove( "hidden" );
 
 
+        for (i = 0; i < answer.length; i++) {
 
-          
+            if (this.innerHTML === answer[i]) {
+                correctThisRound++;
+                correctTotal++;
 
+                // reveals the guessed letter if correct //
+                var reveal = document.getElementById("answer" + i);
+                reveal.classList.remove("hidden");
+
+            } else {
+                wrongThisRound++;
+                console.log(wrongThisRound);
+
+                // if no matches were found //
+                if (wrongThisRound === answer.length) {
+                    wrongTotal++;
+                    alert("bad guess");
+                }
             }
-            else console.log( "wrong!" )
+        }
+
+        // adjusts counter variable //
+        wrongThisRound = 0;
 
 
+        // checks for win or loss condtion //
+
+        if (correctTotal === answer.length) {
+            alert("you win!");
+
+            var gameOver = document.getElementsByClassName("btn");
+            for (i = 0; i < gameOver.length; i++) {
+                gameOver[i].classList.add("invisible");
+            };
+
+            var answerOver = document.getElementsByClassName("letter");
+            for (i = 0; i < answerOver.length; i++) {
+                answerOver[i].classList.remove("hidden");
+
+            };
+        }
+
+        if (wrongTotal === 6) {
+            alert("you lose!");
+
+            var gameOver = document.getElementsByClassName("btn");
+            for (i = 0; i < gameOver.length; i++) {
+                gameOver[i].classList.add("invisible");
+            };
+
+            var answerOver = document.getElementsByClassName("letter");
+            for (i = 0; i < answerOver.length; i++) {
+                answerOver[i].classList.remove("hidden");
+
+            };
 
         }
 
-        
 
-
-        
     })
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
